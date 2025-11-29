2f57bd8f3468612e59751131214138418f32bb25
agilestarskim <agilestarskim@gmail.com>
2025-11-29 16:57:19 +0900

    Initial Commit

---
+++ exit_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  exit_ios
+//
+//  Created by 김민성 on 11/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ exit_ios/exit_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  exit_iosApp.swift
+//  exit_ios
+//
+//  Created by 김민성 on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct exit_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
