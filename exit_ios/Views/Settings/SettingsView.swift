272f22b1fdfc9a4a7e3c1fbc213bd8583ed7b887
agilestarskim <agilestarskim@gmail.com>
2025-12-08 19:30:26 +0900

    버전 수정

--- exit_ios/Views/Settings/SettingsView.swift
+++ exit_ios/Views/Settings/SettingsView.swift
@@ -325,7 +325,7 @@ struct SettingsView: View {
                 
                 Spacer()
                 
-                Text("1.0.0")
+                Text("1.0.1")
                     .font(.Exit.body)
                     .foregroundStyle(Color.Exit.tertiaryText)
             }
